f6f86002754a2c67b4b8a0512ca93ee415b97bde
Lou Amadio <lamadio@microsoft.com>
2020-03-25 16:35:04 -0700

    final updates

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # jackal_firmware_ms
+This repository is for Firmware updates for the Clearpath Jackal using ROS on Windows.
+
+To perform the firmware update:
+1. Please the jackal hardware into firmware update mode.
+1. source the ROS environment
+1. run `rosrun jackal_firmware_ms scripts/upload.bat`
+1. follow the onscreen prompts
+1. shutdown Windows, then reset the DFU switch
+1. restart the Robot
+
+
+
--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,13 +1,29 @@
 @echo off
 for /F "tokens=* USEBACKQ" %%g in (`catkin_find jackal_firmware_ms firmware\mcu.bin`) do (SET bin_file=%%g)
 
-echo In order to update the firmware, A User interface utility called ZaDig will be launched.
-echo Since it does not have a command line interface, please select *Replace Driver*
-pause
-zadig
+rem this is done here because the hosted dfu-util package has not been updated to with the checksum
+choco install dfu-util -y --allow-empty-checksums
 
-echo Continue once Zadig exits
+rem doing this here as well so that it doesn't need to resovle through rosdep
+choco install zadig -y
+
+echo If you have not already done so, please put the hardware into firmware update mode. 
+pause
+
+
+echo In order to update the firmware, A User interface utility called ZaDig will be launched.
+echo Since it does not have a command line interface, please select 'Replace Driver'.
+pause
+
+rem pushing the current path so zadig can find the ini file
+pushd %~dp0
+zadig
+popd 
+
+echo Please continue after Zadig exits
 pause
 
 echo "Uploading file: %BIN_FILE%"
 dfu-util -v -d 0483:df11 -a 0 -s 0x08000000 -D %BIN_FILE%
+
+echo Please shutdown Windows, then flip the bootloader switch and restart. 
